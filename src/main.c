d8bf483d04c6693f8b1b019a92c734eea223d26e
rubyy24 <janrubas@post.cz>
2021-11-21 11:40:34 +0100

    saving and sorting id nodes to array
    expected free

--- src/main.c
+++ src/main.c
@@ -6,12 +6,28 @@
 /* Predpokladana delka radky */
 #define LINE_LEN 256
 #define DELIMITER ","
+#define DEFAULT_ITEMS_COUNT 4
 
-void load_nodes(char *file_name) {
+
+
+
+void load_nodes(const char *file_name) {
     /* Souborovy vstup/vystup */
     FILE *fr;
     char line[LINE_LEN];
-    int nodes_count = 0;
+    /* pole vsech uzlu, defaultne nastaveno na "4" prvky, bude se zvetsovat*/
+    int *nodes;
+    /* aktualni delka pole nodes*/
+    int curr_length;
+
+    nodes = (int*)calloc(DEFAULT_ITEMS_COUNT, sizeof(int));
+    if (nodes == NULL) {
+        printf("Memory not allocated.\n");
+        return;
+    }
+    curr_length = DEFAULT_ITEMS_COUNT;
+
+    /* vynulovani znaku v pameti "line" */
     memset(line, 0, LINE_LEN);
 
     //otevreni souboru
@@ -20,16 +36,23 @@ void load_nodes(char *file_name) {
         printf("No Such File !! ");
         return;
     }
-
-
     while (!feof(fr)) {
         fgets(line, LINE_LEN, fr);
         // pokud line obsahuje "id", automaticky se rozumi, ze jde o prvni radku, rovnou se preskoci
         if (strstr(line, "id") == NULL){
-
             char *token = strtok(line, DELIMITER);
             if (token != NULL) {
-                printf("%d\n", atoi(token));
+                // TODO dodelat osetreni konverze
+                int tmp = atoi(token);
+
+                if (tmp < curr_length){
+                    nodes[tmp] = tmp;
+                    //printf("%d\n",nodes[tmp]);
+                } else {
+                    curr_length = tmp +1;
+                    nodes = realloc(nodes, (tmp+1) * sizeof(int));
+                    nodes[tmp] = tmp;
+                }
             }
 
         }
@@ -42,10 +65,16 @@ void load_nodes(char *file_name) {
         return;
     }
 
+    int i;
+
+    for (i = 0; i < curr_length; i++) {
+        printf("%d\n", nodes[i]);
+    }
+
 }
 
 
-void load_edges(char *file_name) {
+void load_edges(const char *file_name) {
     /* Souborovy vstup/vystup */
     FILE *fr;
     char line[LINE_LEN];
@@ -95,8 +124,8 @@ void load_edges(char *file_name) {
 int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
 
-//    load_nodes("./data/plzen/pilsen_nodes.csv");
-    load_edges("./data/plzen/pilsen_edges.csv");
+    load_nodes("./data/plzen/pilsen_nodes.csv");
+//    load_edges("./data/plzen/pilsen_edges.csv");
 
     return 0;
 }
